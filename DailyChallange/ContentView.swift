9d5cbbf8a3a4df8f9ead937b774306af4af17f3f
Daniele Donia <d.donia@studenti.unisa.it>
2022-07-14 11:37:30 +0200

    Inizio interfaccia grafica

--- DailyChallange/ContentView.swift
+++ DailyChallange/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    /*init(){
+        UINavigationBar.appearance()
+            .setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+        UINavigationBar.appearance().shadowImage = UIImage()
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().barTintColor = .systemGroupedBackground
+    }*/
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+    ScrollView(showsIndicators: false){
+        VStack{
+            Text("Welcome Back Harald")
+                .font(.custom("Avenir-Heavy", size: 30))
+            Text("Ready to start your day")
+                .font(.custom("Avenir-Heavy", size: 18))
+            HStack{
+                Spacer()
+            }
+        }.padding(24)
+    }
+    .background(Color(.systemGroupedBackground)
+        .ignoresSafeArea())
+    .navigationBarTitleDisplayMode(.inline)
+    .navigationBarItems(leading: Button{
+        
+    } label: {
+        Image(systemName: "list.bullet")
+    }, trailing: Button{
+        
+    } label: {
+        Image(systemName: "bell")
+    })
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView{
+            ContentView()
+        }.accentColor(.primary)
     }
 }
