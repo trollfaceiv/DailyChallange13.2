ac47d5eb44dd2e9a93bcc7cbfd1fb2a60fe79291
Gennaro Rascato <g.rascato@icloud.com>
2022-07-14 10:43:07 +0200

    Initial Commit

---
+++ DailyChallange/DailyChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyChallangeApp.swift
+//  DailyChallange
+//
+//  Created by Gennaro Rascato on 14/07/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
