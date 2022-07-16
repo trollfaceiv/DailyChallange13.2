a5b1ae3e236d456e7963e63cf8619efc04b8d524
Daniele Donia <d.donia@studenti.unisa.it>
2022-07-16 20:13:29 +0200

    Aggiunta pagina di registrazione e login

---
+++ DailyChallange/LoginView.swift
@@ -0,0 +1,130 @@
+//
+//  SignInView.swift
+//  DailyChallange
+//
+//  Created by Daniele Donia on 15/07/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    @State private var iconColor : Color = Color(.systemBlue)
+    
+    var body: some View {
+        NavigationView{
+            ZStack{
+                Color.cyan.opacity(0.7).ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                VStack{
+                    Image("login").resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 300)
+                    Text("Sign in")
+                        .font(.largeTitle)
+                        .bold()
+                    
+                    VStack(spacing: 20){
+                        CustomTextField(image: "person", placeHolder: "Username", txt: $username, bgColor: iconColor)
+                        
+                        CustomTextField(image: "lock", placeHolder: "Password", txt: $password, bgColor: iconColor)
+                        
+                        Button("SIGN IN"){
+                            authenticateUser(username: username, password: password)
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 300, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        
+                        
+                    }
+                    
+                    
+                    
+                    
+                    
+                    NavigationLink(
+                        destination: Text("You are loggeed in @\(username)"), isActive: $showingLoginScreen){
+                            EmptyView()
+                        }
+                    Spacer().frame(width: 400, height: 350)
+                }
+            }
+        }
+        .navigationBarHidden(true)
+    }
+    func authenticateUser(username: String, password: String){
+        iconColor = Color(.systemBlue)
+        if username.lowercased() == "mario2021"{
+            wrongUsername = 0
+            
+            if password.lowercased() != "abc123"{
+                wrongPassword = 2
+                iconColor = Color(.systemRed)
+                
+            }else {
+                wrongPassword = 0
+                showingLoginScreen = true
+            }
+            
+        }else{
+            wrongUsername = 2
+            iconColor = Color(.systemRed)
+        }
+        
+       
+    }
+}
+
+struct CustomTextField : View {
+    var image : String
+    var placeHolder: String
+    @Binding var txt : String
+    var bgColor : Color
+    
+    var body: some View{
+        ZStack(alignment: Alignment(horizontal: .leading, vertical: .center)) {
+        
+            Image(systemName: image)
+                .font(.system(size: 24))
+                .foregroundColor(Color.white)
+                .frame(width: 60, height: 60)
+                .background(bgColor)
+                .clipShape(Circle())
+            
+            ZStack{
+                if placeHolder == "Password" || placeHolder == "Re-enter"{
+                    SecureField(placeHolder, text: $txt)
+                }
+                else {
+                    TextField(placeHolder, text: $txt)
+                }
+            }
+                
+                
+                .padding(.horizontal)
+                .padding(.leading, 65)
+                .frame(height: 60)
+                .background(Color(.systemGray5).opacity(0.2))
+                .clipShape(Capsule())
+            
+        
+        }.padding(.horizontal)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
--- DailyChallange/SignUpView.swift
+++ DailyChallange/SignUpView.swift
@@ -1,15 +1,105 @@
 //
-//  SignUpView.swift
+//  SignInView.swift
 //  DailyChallange
 //
-//  Created by Pasquale Somma on 16/07/22.
+//  Created by Daniele Donia on 15/07/22.
 //
 
 import SwiftUI
 
 struct SignUpView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    @State private var iconUserColor : Color = Color(.systemBlue)
+    @State private var iconPasswordColor : Color = Color(.systemBlue)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            ZStack{
+                Color.cyan.opacity(0.7).ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                VStack{
+                    Image("signup").resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
+                    Text("Sign up")
+                        .font(.largeTitle)
+                        .bold()
+                    
+                    VStack(spacing: 20){
+                        CustomTextField(image: "person", placeHolder: "Username", txt: $username, bgColor: iconUserColor)
+                        
+                        CustomTextField(image: "lock", placeHolder: "Password", txt: $password, bgColor: iconPasswordColor)
+                        
+                        CustomTextField(image: "lock", placeHolder: "Re-enter", txt: $confirmPassword, bgColor: iconPasswordColor)
+                        VStack{
+                            Text("Wellness level")
+                        IntSlider().padding(.horizontal).frame(width: 300, height: 50)
+                        }
+                        
+                        Button("SIGN UP"){
+                            authenticateUser(username: username, password: password)
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 300, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        
+                        
+                    }
+                    
+                    
+                    
+                    
+                    
+                    NavigationLink(
+                        destination: Text("You are loggeed in @\(username)"), isActive: $showingLoginScreen){
+                            EmptyView()
+                        }
+                    Spacer().frame(width: 400, height: 250)
+                }
+            }
+        }
+        .navigationBarHidden(true)
+    }
+    func authenticateUser(username: String, password: String){
+        iconPasswordColor = Color(.systemBlue)
+        if !password.elementsEqual(confirmPassword){
+            iconPasswordColor = Color(.systemRed)
+        }
+        
+        
+    }
+}
+
+struct IntSlider: View {
+    @State var score: Int = 0
+    var intProxy: Binding<Double>{
+        Binding<Double>(get: {
+            //returns the score as a Double
+            return Double(score)
+        }, set: {
+            //rounds the double to an Int
+            print($0.description)
+            score = Int($0)
+        })
+    }
+    var body: some View {
+        VStack{
+            Text(score.description)
+            Slider(value: intProxy , in: 0.0...5.0, step: 1.0, onEditingChanged: {_ in
+                print(score.description)
+            })
+        }
     }
 }
 
